f3deaa603c382c1a0b77bb1bc7861a70fc860bc0
Jacques Belmont <jacquesbelmont@gmail.com>
2014-03-14 21:20:48 -0300

    Modificação Teste 002

--- assets/js/controllers/MapaController.js
+++ assets/js/controllers/MapaController.js
@@ -2,19 +2,22 @@ var MapaController = function() {};
 
 MapaController.prototype = {
     initialize: function() {
-   
-     
         
-        //INICIO DO MAPA
-      
-        
-        //FIM DO MAPA
-        
-        
+   var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
         
     },
     destroy: function() {
-        map.remove();
+        google.maps.event.removeListener();
 
         alert("Foi destruido!");
 
