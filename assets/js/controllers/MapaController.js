8f5a552cff1784e6216299a0e6e40e78eb42ad9c
Jacques Belmont <jacquesbelmont@gmail.com>
2014-05-21 23:04:18 -0300

    Update MapaController.js
    Correção baseado nesse tópico http://pt.stackoverflow.com/questions/9405/problema-no-google-maps-javascript-api-v3-com-webapp-em-mvc

--- assets/js/controllers/MapaController.js
+++ assets/js/controllers/MapaController.js
@@ -14,8 +14,8 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
-       
+//google.maps.event.addDomListener(window, 'load', initialize);
+       initialize();
 //FIM
         
     },
